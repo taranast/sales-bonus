60c4650e5c602dab327ce06b5280fdf733b46c6c
taranast <taranast434@gmail.com>
2026-04-25 16:49:29 +0300

    feat: check input data and functions in options

--- src/main.js
+++ src/main.js
@@ -26,10 +26,25 @@ function calculateBonusByProfit(index, total, seller) {
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
+    if (!data
+        || !Array.isArray(data.sellers)
+        || !Array.isArray(data.products) 
+        || !Array.isArray(data.purchase_records)
+        || data.sellers.length === 0 
+        || data.products.length === 0 
+        || data.purchase_records.length === 0
+        ) {
+            throw new Error('Некорректные входные данные');
+        }
 
-    // @TODO: Проверка наличия опций
-
+    const { calculateRevenue, calculateBonus } = options;
+    if (!calculateRevenue 
+        || !calculateBonus 
+        || typeof calculateRevenue !== "function" 
+        || typeof calculateBonus !== "function"
+        ) {
+            throw new Error('Чего-то не хватает');
+        }
     // @TODO: Подготовка промежуточных данных для сбора статистики
 
     // @TODO: Индексация продавцов и товаров для быстрого доступа
